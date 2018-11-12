646f5571ab676f402fb2f6d56a52c612c1c2eeb0
gly19930203 <601291459@qq.com>
2018-11-12 21:56:26 +0800

    201811122156

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ Vue.use(Vuex)
 
 //global.APP_HOST = 'http://192.168.1.9:8020'//手机扫码功能，微信端路径
 //global.IMG_HOST = 'http://192.168.1.9:8006'
-axios.defaults.baseURL = 'http://192.168.0.10:8006/back';
+axios.defaults.baseURL = 'http://192.168.0.8:8006/back';
 
  global.APP_HOST = 'http://www.edutage.com.cn/crm/marketing'
  global.IMG_HOST = 'http://crmapi.edutage.com.cn'
